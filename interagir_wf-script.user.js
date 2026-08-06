444c10dc5ad59f535a6b97c73ae5c8806fb814b7
elitonnairnei-barigui <eliton.nairnei@barigui.com.br>
2026-08-06 14:12:30 -0300

    Update interagir_wf-script.user.js

--- interagir_wf-script.user.js
+++ interagir_wf-script.user.js
@@ -1,13 +1,19 @@
 // ==UserScript==
-// @name         Meu Script GitHub
+// @name         AjustarJanelaWF
 // @namespace    https://github.com/seu-usuario
 // @version      1.0.0
 // @description  Exemplo de script hospedado no GitHub
-// @author       Seu Nome
-// @match        https://example.com/*
+// @author       Elitonn
+// @match        https://wf.grupobarigui.com.br/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=grupobarigui.com.br
 // @downloadURL  https://raw.githubusercontent.com/seu-usuario/meu-repo/main/meu-script.user.js
 // @updateURL    https://raw.githubusercontent.com/seu-usuario/meu-repo/main/meu-script.user.js
 // @grant        none
 // ==UserScript==
+
+
+
+
+
 
 console.log("Script carregado via GitHub!");

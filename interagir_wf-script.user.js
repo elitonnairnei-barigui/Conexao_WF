0f56531d6782e88422b81054fca551f90aeecbf9
elitonnairnei-barigui <eliton.nairnei@barigui.com.br>
2026-08-06 14:08:31 -0300

    Create interagir_wf-script.user.js

---
+++ interagir_wf-script.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Meu Script GitHub
+// @namespace    https://github.com/seu-usuario
+// @version      1.0.0
+// @description  Exemplo de script hospedado no GitHub
+// @author       Seu Nome
+// @match        https://example.com/*
+// @downloadURL  https://raw.githubusercontent.com/seu-usuario/meu-repo/main/meu-script.user.js
+// @updateURL    https://raw.githubusercontent.com/seu-usuario/meu-repo/main/meu-script.user.js
+// @grant        none
+// ==UserScript==
+
+console.log("Script carregado via GitHub!");

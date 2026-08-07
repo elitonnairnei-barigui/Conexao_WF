9556e3bcad0e9fb1cb891f50cd1754e660ac580f
elitonnairnei-barigui <eliton.nairnei@barigui.com.br>
2026-08-06 23:13:56 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -31,39 +31,39 @@
 		});
 
 
+
+	
+
+	/////////////////////////////////////////
+		// 1. Localiza a linha (tr) dentro da tabela TABLEEMPRESAMARCA
+		const linha = document.querySelector('#TABLEEMPRESAMARCA tr');
+		
+		if (linha) {
+		    // 2. Cria a nova célula <td>
+		    const novoTd = document.createElement('td');
+		    
+		    // 3. Define o conteúdo que vai dentro dela (pode ser texto ou HTML)
+		    novoTd.innerHTML = '<span>Empresa CNPJ</span>'; 
+		    
+			// 4. Insere a nova <td> no final da <tr>
+		    linha.appendChild(novoTd);
+	
+			const novoEmpresaCNPJ = `<select id="meuSelectCustomizado">
+			        <option value="0" selected="selected">Todas</option>
+			        <option value="7">Teste1</option>
+			        <option value="237">Teste2</option>
+			        <option value="203">Teste3</option>
+			        <option value="10">Teste4</option>
+			        <option value="1">Teste5</option>
+			    </select>`;
+			linha.insertAdjacentHTML('beforeend', `<td>${novoEmpresaCNPJ}</td>`);
+			
+		    // (Opcional) Adicionar classes ou estilos na nova td:
+		    // novoTd.className = 'minha-classe';
+		    // novoTd.style.backgroundColor = '#4CAF50';
+		}	
 		// Configura para observar a inclusão/remoção de elementos filhos
 		observer.observe(selecionado, { childList: true, subtree: true });
 	}
-
-/////////////////////////////////////////
-// 1. Localiza a linha (tr) dentro da tabela TABLEEMPRESAMARCA
-	const linha = document.querySelector('#TABLEEMPRESAMARCA tr');
-	
-	if (linha) {
-	    // 2. Cria a nova célula <td>
-	    const novoTd = document.createElement('td');
-	    
-	    // 3. Define o conteúdo que vai dentro dela (pode ser texto ou HTML)
-	    novoTd.innerHTML = '<span>Empresa CNPJ</span>'; 
-	    
-		// 4. Insere a nova <td> no final da <tr>
-	    linha.appendChild(novoTd);
-
-		const novoEmpresaCNPJ = `<select id="meuSelectCustomizado">
-		        <option value="0" selected="selected">Todas</option>
-		        <option value="7">Teste1</option>
-		        <option value="237">Teste2</option>
-		        <option value="203">Teste3</option>
-		        <option value="10">Teste4</option>
-		        <option value="1">Teste5</option>
-		    </select>`;
-		linha.insertAdjacentHTML('beforeend', `<td>${novoEmpresaCNPJ}</td>`);
-		
-	    // (Opcional) Adicionar classes ou estilos na nova td:
-	    // novoTd.className = 'minha-classe';
-	    // novoTd.style.backgroundColor = '#4CAF50';
-	
-
-	}	
 	
 })();
